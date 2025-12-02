48028054dc6a87c0c745148aa5c7ca4d8e5fb920
gkosnia163 <niafaskonstantinos@gmail.com>
2025-12-02 22:24:38 +0200

    insert complete

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 
 #define name_length 50
-#define EAN_length 14
+#define EAN_maxSize 14
 
 typedef struct inventory{
-    char EAN[EAN_length];
+    char EAN[EAN_maxSize];
     char name[name_length];
     int quantity;
     double price; 
@@ -14,7 +14,7 @@ typedef struct inventory{
 }inv;
 
 typedef struct orderItem{
-    char EAN[EAN_length];
+    char EAN[EAN_maxSize];
     int quantity;
     int priority;
     struct orderItem* next;//sxoliase TODO
@@ -37,7 +37,7 @@ typedef struct costumer{
     struct costumer* next; //sxoliase TODO
 }costumer;
 
-inv* newInventoryNode(char EAN[EAN_length], char name[], int quantity, double price){ //δημιουργία καινούριου node     
+inv* newInventoryNode(char EAN[], char name[], int quantity, double price){ //δημιουργία καινούριου node     
     inv* node = (inv*)malloc(sizeof(inv));
     if(!node) return NULL;
 
@@ -50,43 +50,96 @@ inv* newInventoryNode(char EAN[EAN_length], char name[], int quantity, double pr
     return node;
 } 
 
-void insertInventory(inv** head, inv* newInventoryNode){
-    if((*head == NULL) && sizeof(newInventoryNode->EAN) < 4 || sizeof(newInventoryNode->EAN) > 13){  //elegxos ama sizeof(EAN) > 4 && < 13 
-        perror("insufficient amount of 'EAN'(barcode) digits");
-        return;
-    }
-    
-    if(*head == NULL && sizeof(newInventoryNode->EAN) > 4){
-        *head = newInventoryNode;
-        return;
-    }
-
-    inv* temp = *head;
-    while(temp->next != NULL){
-        temp = temp->next;
-    }
-    temp->next = newInventoryNode;
-}
-
-void recommend(inv** head, inv* newInventoryNode, char EAN[]){
-    int i = 0, counter = 0;
+//χρησιμοποιείται στην insertInventory σε περίπτωση που ο χρήστης εισάγει 4-13 ψηφεία
+//επρεπε να μπει πίσω απο insert, αλλιώς δεν την διάβαζε.
+void recommend(inv** head, char EAN[]){ 
     int found = 0;
-    for(i; EAN[i] == '\0'; i++){
-        counter++;
-    }
+    size_t counter = strlen(EAN);
 
-    inv* temp = *head;
-    while(temp->next != NULL){
-        if(strncmp(temp->EAN,EAN,counter)){
+    inv* temp = *head; //αρχικοποίηση temp στο πρώτο node της λίστας
+    while(temp != NULL){ //proteinie edw lathos den kserwz
+        if(strncmp(temp->EAN,EAN,counter) == 0){
             printf("EAN: %s, name: %s, quantity: %d\n",temp->EAN,temp->name,temp->quantity);
             found = 1;
         }
+        temp = temp -> next;
     }
     if(!found) printf("No products found with the given EAN(barcode) code");
 }
 
-int main(){
+void insertInventory(inv** head, char EAN[], char name[], int quantity, double price){ //δεν είναι σκέτη insert, κάνει update και ήδη υπάρχοντα προϊόντα.
+    size_t eanLength = strlen(EAN);//kltra bale size_t
+    if(eanLength < 4 || eanLength > 13){  //Γενικός έλεγχος ψηφίων ΕΑΝ
+        perror("insufficient amount of EAN/barcode digits");
+        return;
+    }
+    
+    inv* temp = *head; //αρχικοποίηση στο πρώτο node της λίστας
+    while(temp != NULL){ //αναζήτηση ήδη υπαρκτής ΕΑΝ
+        if(strcmp(temp->EAN, EAN) == 0){ //αμα υπάρχει
+            temp->quantity += quantity; //αυξάνει την ποσότητα μόνο
+            return;
+        }
+        temp = temp->next;
+    }
 
+    //περίπτωση που δεν πληρούνται τα ψηφία όλα, αλλα υπάρχουν αρκετά για recommendation
+    if(eanLength < 13){ 
+        printf("\nProducts with similar EAN/barcode\n");
+        recommend(head, EAN);
+    }
+    //περίπτωση που πληρούνται όλες οι προϋποθέσεις και δημιουργήτε νέο node (προϊόν), τύπου
+    //inv* με την βοήθεια της newInventoryNode συνάρτησης    
+    else if(eanLength == 13) { 
+        inv* node = newInventoryNode(EAN, name, quantity, price);
+
+        if(*head == NULL){ //θα βάλουμε το καινούριο node στο τέλος της λίστας,
+            *head = node;
+        }else{
+            temp = *head;
+            while(temp->next != NULL){
+                temp = temp->next;
+            }            
+            temp->next = node; //εισαγωγή του νέου node
+        }
+        printf("Product : '%s' (EAN: %s), added to inventory\n", name, EAN);
+    }
+}
+
+/*Εισαγωγή νέου προϊόντος (αν δεν υπάρχει το προϊόν το προσθέτει στο απόθεμα 
+αλλιώς ανανεώνει το απόθεμα για το συγκεκριμένο προϊόν). Θα πρέπει να γίνεται 
+έλεγχος στο ΕΑΝ και είτε να εμφανίζεται μήνυμα λάθους είτε -εάν είναι μικρότερο 
+από 13 αλλά τουλάχιστον 4 χαρακτήρες- να προτείνει κωδικούς προϊόντων που 
+αρχίζουν με το ίδιο πρόθεμα για να επιλέξει ο χρήστης. */
+
+int main(){     
+    inv* invHead = NULL;
+    char line[100]; //= {"nothing yet"};
+    char word[20];
+
+    while(fgets(line,sizeof(line), stdin) != NULL){
+
+        sscanf(line, "%s", word);
+        if(strcmp(word, "insert") == 0){
+            char ean[EAN_maxSize];
+            char name[name_length];
+            int quantity;
+            double price;
+
+            if(sscanf(line,"%*s %s %s %d %lf", ean, name, &quantity, &price) == 4){ //χρειάστηκε μια extra δήλωση τύπου δεδομένων *s για να προσπεράσουμε την πρώτη λέξη 'insert'
+                insertInventory(&invHead, ean, name, quantity, price);
+            }else printf("Wrong insert format, try: 'insert <barcode> <name> <quantity> <price>'\n");
+
+        }else if (strcmp(word, "print") == 0){
+            inv* temp = invHead;
+            while(temp != NULL){
+                printf("EAN: %s, name: %s, quantity: %d, price: %.2lf\n", temp->EAN, temp->name, temp->quantity, temp->price);
+                temp = temp->next;
+            }
+        }else if(strcmp(word, "exit") == 0) {
+            break;
+        }else printf("Cannot recognise command, type help\n"); // kanto kltro grapse sxolia TODO
+    }
 
     return 0;   
 }

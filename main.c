1296e44354fdf3d62062e540e642175f788c94dc
gkosnia163 <niafaskonstantinos@gmail.com>
2025-12-18 23:41:44 +0200

    finished ffs

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+//github repo: https://github.com/gkosnia163/ergasia1-domes
+
+/*
+Κωνσταντίνος Ιάκωβος Νιάφας
+2022202100140
+dit21140@go.uop.gr
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -6,37 +14,40 @@
 #define name_length 50
 #define EAN_maxSize 14
 
+//απλή singley linked λίστα που περιέχει σοτιχεία τύπου inventory
 typedef struct inventory{
     char EAN[EAN_maxSize];
     char name[name_length];
     int quantity;
     double price; 
-    struct inventory* next;//sxoliase TODO
+    struct inventory* next;
 }inv;
 
+//απλή singley linked λίστα που περιέχει στοιχεία τύπου οrder
 typedef struct order{
     char costumer_id[8];
     char buisness_name[name_length];
     char EAN[EAN_maxSize];
     int quantity;
     int priority;
-    struct order* next;//sxoliase TODO
+    struct order* next;
 }order;
 
-/* αυτό το struct αντιπροσωπεύει ουρά προτεραιότητας, θα χρησιμοποιείται μόνο για enqueue, οπότε το *head αρκεί.*/
+//αυτό το struct αντιπροσωπεύει ουρά προτεραιότητας, θα χρησιμοποιείται μόνο για enqueue, οπότε το *head αρκεί.
 typedef struct orderPriority{ 
     order* head;
 }orderPriority;
 
-//extra monada
+//extra μονάδα λίστα (singly linked) των πελατών
 typedef struct costumer{
     char costumer_id[8];
     char buisness_name[name_length];
     char phone[10];
     char address[name_length];
-    struct costumer* next; //sxoliase TODO
+    struct costumer* next; 
 }costumer;
 
+//χρησιμοποιείται στην insert για την δημιουργία νέων node (λόγω "καθαρότητας")
 inv* newInventoryNode(char EAN[], char name[], int quantity, double price){ 
     inv* node = (inv*)malloc(sizeof(inv));
     if(!node) return NULL;
@@ -50,6 +61,7 @@ inv* newInventoryNode(char EAN[], char name[], int quantity, double price){
     return node;
 } 
 
+//παρομοίως χρησιμοποιείται στην new order
 order* newOrderNode(char id[], char costumer_name[], char EAN[], int quantity, int priority){
     order* node = (order*)malloc(sizeof(order));
     if(!node) return NULL;
@@ -65,13 +77,12 @@ order* newOrderNode(char id[], char costumer_name[], char EAN[], int quantity, i
 }
 
 //χρησιμοποιείται στην insertInventory σε περίπτωση που ο χρήστης εισάγει 4-13 ψηφεία
-//επρεπε να μπει πίσω απο insert, αλλιώς δεν την διάβαζε.
 void recommend(inv** head, char EAN[]){ 
     int found = 0;
     size_t counter = strlen(EAN);
 
     inv* temp = *head;                         //αρχικοποίηση temp στο πρώτο node της λίστας
-    while(temp != NULL){ //proteinie edw lathos den kserwz
+    while(temp != NULL){ 
         if(strncmp(temp->EAN, EAN,counter) == 0){
             printf("EAN: %s, name: %s, quantity: %d\n",temp->EAN,temp->name,temp->quantity);
             found = 1;
@@ -81,6 +92,7 @@ void recommend(inv** head, char EAN[]){
     if(!found) printf("No products found with the given EAN(barcode) code\n");
 }
 
+//έλεγχος της sscanf άμα δόθηκαν σωστά οι createAccount() παράμετροι 
 int checkAcc(costumer** head, char id[], char name[]){
     size_t idLength = strlen(id), nameLength = strlen(name);
 
@@ -88,6 +100,7 @@ int checkAcc(costumer** head, char id[], char name[]){
         printf("invalid id/name\n");
         return -1;
     }
+
     costumer* temp = *head;
     while(temp != NULL){
         if(strcmp(temp->buisness_name, name) == 0){
@@ -99,6 +112,7 @@ int checkAcc(costumer** head, char id[], char name[]){
     return 1;
 }
 
+//έλεγχος της sscanf της insert ... επιλογής
 int checkInsert(inv** head, char EAN[], char name[]){
     size_t eanLength = strlen(EAN);
     if(eanLength < 4 || eanLength > 13){      //έλεγχος πλήθους ψηφίων ΕΑΝ
@@ -122,9 +136,11 @@ int checkInsert(inv** head, char EAN[], char name[]){
         }
         temp = temp->next;
     }
-    return 1;                                 //όλα πήγαν καλά
+    return 1;                                 
 }
 
+//δημιουργεί λογαριασμού, δημιουργείται νεο node τύπου costumer και εισάγονται τα δεδομένα μέσα στο node
+//χρησιμοποιείται checkAcc() συνάρτηση
 void createAccount(costumer** head, char id[], char name[], char phone[], char address[]){
     costumer* node = (costumer*)malloc(sizeof(costumer));
     if(!node) return;
@@ -144,10 +160,12 @@ void createAccount(costumer** head, char id[], char name[], char phone[], char a
     return;
 }
 
-void insertInventory(inv** invHead, char EAN[], char name[], int quantity, double price){ //δεν είναι σκέτη insert, κάνει update και ήδη υπάρχοντα προϊόντα.
+//insert new node (τύπου inventory) + κάνει update και ήδη υπάρχοντα προϊόντα.
+//χρησιμοποιούνται checkInsert(), newInventoryNode(), recommend() συναρτήσεις
+void insertInventory(inv** invHead, char EAN[], char name[], int quantity, double price){ 
     size_t eanLength = strlen(EAN);
     
-    if(checkInsert(invHead, EAN, name) == -1) return;            //ελεγχος length κωδικού ΕΑΝ και ονόματος
+    if(checkInsert(invHead, EAN, name) == -1) return;      //ελεγχος length κωδικού ΕΑΝ και ονόματος
 
     inv* temp = *invHead;                      //αρχικοποίηση στο πρώτο node της λίστας
     while(temp != NULL){                       //αναζήτηση λίστας 
@@ -166,7 +184,7 @@ void insertInventory(inv** invHead, char EAN[], char name[], int quantity, doubl
     }
 
     //περίπτωση που πληρούνται όλες οι προϋποθέσεις και δημιουργήτε νέο node (προϊόν), τύπου
-    //inv* με την βοήθεια της newInventoryNode συνάρτησης    
+    //inv* με την βοήθεια της newInventoryNode() συνάρτησης    
     else if(eanLength == 13) { 
         inv* node = newInventoryNode(EAN, name, quantity, price);
 
@@ -179,13 +197,28 @@ void insertInventory(inv** invHead, char EAN[], char name[], int quantity, doubl
             }            
             temp->next = node;                     //εισαγωγή νέου node
         }
-        printf("Product : '%s' (EAN: %s), added to inventory\n", name, EAN);
+        printf("Product: '%s' (EAN: %s), added to inventory\n", name, EAN);
     }
 }
 
-/*θα προσθέτουμε τα προϊόντα παραγγελίας με όνομα αντί να γράφει ο χρήστης 14ψήφιους αριθμούς. Update: θα βάλουμε και enqueue
-απευθείας στην newOrder, ώστε να γίνεται sort η λίστα των τύπου order μεταβλητών με ουρά προτεραιότητας*/
-void newOrder(orderPriority* oP, inv* invHead, char id[], char buisnessName[], char name[],int quantity, int priority){
+//δημιουργία νέου node τύπου order με newOrderNode συνάρτηση
+//κάνουμε enqueue απευθείας στην newOrder, ώστε να γίνεται sort η λίστα (των τύπου order μεταβλητών) με ουρά προτεραιότητας
+//χρησιμοποιείται newOrderNode() συνάρτηση
+void newOrder(orderPriority* oP, inv* invHead, costumer* accHead, char id[], char buisnessName[], char name[],int quantity, int priority){
+    int found = 0;
+    costumer* tempAcc = accHead;
+    while(tempAcc != NULL){
+        if(strcmp(tempAcc->costumer_id, id) == 0 && strcmp(tempAcc->buisness_name, buisnessName) == 0){
+            found = 1;
+            break;
+        }
+        tempAcc = tempAcc->next;
+    }
+    if(!found){
+        printf("Order rejected: wrong id: %s or name: %s\n", id, buisnessName);
+        return;
+    }
+
     inv* temp = invHead;
     while(temp != NULL){
             if(strcmp(name, temp->name) == 0 || strcmp(name, temp->EAN) == 0){                              //έλεγχος ΕΑΝ εαν εντάχθηκε σωστά (μαζί και με όνομα)
@@ -212,6 +245,7 @@ void newOrder(orderPriority* oP, inv* invHead, char id[], char buisnessName[], c
 
 /*-  Εκτύπωση αποθέματος (δέχεται είτε κωδικό ή μέρος του είτε όνομα είτε * για 
 εκτύπωση όλου του αποθέματος).  */
+//χρησιμοποιείται συνάρτηση recommend()
 void print(inv** invHeadPtr, order* orderHead,costumer* accHead, char choice[name_length]){
     inv* tempInv = *invHeadPtr;
     if(strcmp(choice, "*") == 0){                    //τύπωση όλης της λίστας inventory
@@ -267,6 +301,7 @@ void print(inv** invHeadPtr, order* orderHead,costumer* accHead, char choice[nam
     }
 }
 
+//επεξεργασία παραγγελιών
 //έχει γίνει ήδη enqueue της order ουράς (απο το priority που δίνει ο χρήστης) μέσα στην newOrder συνάρτηση
 void completeOrders(orderPriority* oP, inv** invHeadPtr){
     if(oP->head == NULL){
@@ -274,62 +309,79 @@ void completeOrders(orderPriority* oP, inv** invHeadPtr){
         return;
     }
     printf("executing..\n\n");
-    while(oP->head != NULL){
-        char currentId[8]; 
-        strcpy(currentId, oP->head->costumer_id);
-        double totalCost = 0;              
-        order* currentOrder = oP->head;
-        order* prevOrder = NULL;
+    
+    order* currentOrder = oP->head;
+    order* prevOrder = NULL;
+    char lastCustomerId[8] = "";
+    double totalCost = 0;
 
-        printf("receipt: %s\n--------------------------------------------------------\n",currentId);
-
-        while(currentOrder != NULL){
-            if(strcmp(currentOrder->costumer_id, currentId) == 0){
-                inv* tempInv = *invHeadPtr;
-                inv* prevInv = NULL;  //Χρησιμοποιούμε την prev μόνο για να μπορούμε να κάνουμε dequeue προϊόντα τα οποία δεν έχουν άλλο quantity στο inventory
-                while(tempInv != NULL){
-                    if(strcmp(tempInv->EAN, currentOrder->EAN) == 0 ){
-                        double thisCost = 0;
-                        if(tempInv->quantity >= currentOrder->quantity){
-                            tempInv->quantity -= currentOrder->quantity;
-                            thisCost = currentOrder->quantity * tempInv->price;
-                            printf("%s -- quantity: %d -- price: %.2lf\n", tempInv->name, currentOrder->quantity, thisCost);
-                        }else{
-                            thisCost = tempInv->quantity * tempInv->price;
-                            printf("%s -- %d%% of available quantity: %d units -- price: %.2lf\n", tempInv->name, (tempInv->quantity*100)/currentOrder->quantity, tempInv->quantity, thisCost);
-                            tempInv->quantity = 0;
-                        }
-                
-                        totalCost += thisCost;
-                        
-                        //Δεν ζητήται αλλά μου φάνηκε λογικό να αφαιρούμε το node, ώστε να φαίνεται πιο καθαρή η λίστα inventory
-                        if(tempInv->quantity == 0){
-                            if(prevInv == NULL){                //άμα βρισκόμαστε στο πρώτο στοιχείο του πίνακα και το διαγράψουμε
-                                *invHeadPtr = tempInv->next;    //πρέπει να ξεκινήσουμε την ουρά από το 2ο στοιχείο, ενημερώνοντας
-                            }else{                              //τον head pointer του inventory 
-                                prevInv->next = tempInv->next;
-                            }
-                            free(tempInv);
-                        }
-                        break;
-                    }
-                    prevInv = tempInv;
-                    tempInv = tempInv->next;
-                }
-                if(prevOrder == NULL){
-                    oP->head = currentOrder->next;
-                    currentOrder = oP->head;
-                }else{
-                    prevOrder = currentOrder->next;
-                    currentOrder = prevOrder->next;
-                }
-                free(currentOrder);
-            }else{
-                prevOrder = currentOrder;
-                currentOrder = currentOrder->next;
+    while(currentOrder != NULL){
+        //αν αλλάξει ο πελάτης, τυπώνουμε σύνολο προηγούμενου και header νέου
+        if(strcmp(currentOrder->costumer_id, lastCustomerId) != 0){
+            if(strlen(lastCustomerId) > 0){                   //ελέγχουμε αμα υπήρχε προηγούμενος πελάτης, αμα strlen(lastCustomerId) == 0, είναι η πρώτη παραγγελία και δεν
+                                                              //τυπώνουμε του προηγούμενου (που δεν υπάρχει)
+                 printf("--------------------------------------------------------\ntotal:\t%.2lf\n\n", totalCost);
             }
+            printf("receipt: %s\n--------------------------------------------------------\n", currentOrder->buisness_name);
+            strcpy(lastCustomerId, currentOrder->costumer_id);//ενημέρωση ότι έχουμε δεί αυτόν τον πελάτη
+            totalCost = 0;
         }
-        printf("--------------------------------------------------------\ntotal:\t%.2lf", totalCost);
+
+        inv* tempInv = *invHeadPtr;
+        inv* prevInv = NULL;
+        
+        while(tempInv != NULL){
+            if(strcmp(tempInv->EAN, currentOrder->EAN) == 0 ){
+                double thisCost = 0;
+                if(tempInv->quantity >= currentOrder->quantity){        //πλήρης κάλυψη ποσότητας παραγγελίας
+                    tempInv->quantity -= currentOrder->quantity;
+                    thisCost = currentOrder->quantity * tempInv->price;
+                    printf("%s -- quantity: %d -- price: %.2lf\n", tempInv->name, currentOrder->quantity, thisCost);
+                    currentOrder->quantity = 0; 
+                }else{                                                  //μερική κάλυψη 
+                    thisCost = tempInv->quantity * tempInv->price;
+                    printf("%s -- %d%% of available quantity: %d units -- price: %.2lf\n", tempInv->name, (tempInv->quantity*100)/currentOrder->quantity, tempInv->quantity, thisCost);
+                    currentOrder->quantity -= tempInv->quantity; // Μείωση ποσότητας παραγγελίας
+                    tempInv->quantity = 0;
+                }
+        
+                totalCost += thisCost;
+                
+                //το ξέρω ότι δεν ζητήται, αλλά μου φάνηκε λογικό να διαγράφεται το συγκεκριμένο (inventory)node άμα φτάσει inventory quantity == 0
+                if(tempInv->quantity == 0){
+                    if(prevInv == NULL){                
+                        *invHeadPtr = tempInv->next;    
+                    }else{                              
+                        prevInv->next = tempInv->next;
+                    }
+                    free(tempInv);                      
+                }                                       
+                break;
+            }
+            prevInv = tempInv;
+            tempInv = tempInv->next;
+        }
+        
+        //διαγραφή παραγγελίας ΜΟΝΟ αν ολοκληρώθηκε (quantity == 0)
+        if(currentOrder->quantity == 0){
+            order* toDelete = currentOrder;             //κρατάμε δείκτη στο node που θα διαγράψουμε
+            if(prevOrder == NULL){              
+                oP->head = currentOrder->next;
+                currentOrder = oP->head;
+            }else{
+                prevOrder->next = currentOrder->next;   //συνδέουμε το προηγούμενο με το επόμενο
+                currentOrder = currentOrder->next;      //προχωράμε στο επόμενο
+            }
+            free(toDelete);                             //αφαίρεση κορυφαίου node της λίστας
+        }else{
+            //αν υπάρχει υπόλοιπο, την κρατάμε και προχωράμε
+            prevOrder = currentOrder;
+            currentOrder = currentOrder->next;
+        }
+    }
+    
+    if(strlen(lastCustomerId) > 0){
+         printf("--------------------------------------------------------\ntotal:\t%.2lf\n", totalCost);
     }
 }
 
@@ -341,6 +393,7 @@ int main(){
     char line[100]; //= {"nothing yet"};
     char word[20];
 
+    //Όλες οι επιλογές γίνονται με sscanf ώστε ο χρήστης να εισάγει 1 εντολή για κάθε λειτουργία
     while(fgets(line,sizeof(line), stdin) != NULL){
         sscanf(line, "%s", word);
         
@@ -350,7 +403,8 @@ int main(){
             char phone[10];
             char address[name_length];
 
-            if(sscanf(line, "%*s %s %s %s %s", id, buisness_name, phone, address) == 4){ ///////////to allaxa
+            //χρειάστηκε μια extra δήλωση τύπου δεδομένων *s για να προσπεράσουμε την πρώτη λέξη 'insert'
+            if(sscanf(line, "%*s %s %s %s %s", id, buisness_name, phone, address) == 4){ //άμα σκαναριστούν 4 μεταβλητές true
                 createAccount(&accHead, id, buisness_name, phone, address);
             }else printf("Wrong create format, try: 'create <id> <username> <phone> <address>'\n");
         }else if(strcmp(word, "insert") == 0){
@@ -359,8 +413,7 @@ int main(){
             int quantity;
             double price;
             
-            //bres allo solution anti gia *s
-            if(sscanf(line,"%*s %s %s %d %lf", ean, name, &quantity, &price) == 4){ //χρειάστηκε μια extra δήλωση τύπου δεδομένων *s για να προσπεράσουμε την πρώτη λέξη 'insert'
+            if(sscanf(line,"%*s %s %s %d %lf", ean, name, &quantity, &price) == 4){ 
                 insertInventory(&invHead, ean, name, quantity, price);
             }else
                 printf("Wrong insert format, try: 'insert <barcode> <product name> <quantity> <price>'\n");
@@ -373,7 +426,7 @@ int main(){
             int priority;
 
             if(sscanf(line, "%*s %s %s %s %d %d", id, buisnessName, name, &quantity, &priority) == 5){
-                newOrder(&orderHead, invHead, id, buisnessName, name, quantity, priority);
+                newOrder(&orderHead, invHead, accHead, id, buisnessName, name, quantity, priority);
             }else
                 printf("Wrong order format, try: 'order <id> <username> <EAN> <quantity> <priority>'\n");
         
@@ -389,14 +442,30 @@ int main(){
             completeOrders(&orderHead, &invHead);
             
         }else if(strcmp(word, "exit") == 0){
-            free(invHead);                  
-            free(orderHead.head);        ///////////////////////////////////fucken fix that shi        
+            inv* tempInv;
+            while(invHead != NULL){
+                tempInv = invHead;
+                invHead = invHead->next;
+                free(tempInv);
+            }
+            order* tempOrder;
+            while(orderHead.head != NULL){
+                tempOrder = orderHead.head;
+                orderHead.head = orderHead.head->next;
+                free(tempOrder);
+            }
+            costumer* tempAcc;
+            while(accHead != NULL){
+                tempAcc = accHead;
+                accHead = accHead->next;
+                free(tempAcc);
+            }
             break;
 
         }else if(strcmp(word, "help") == 0){
-            printf("Command list:\n\n\t- insert <barcode> <name> <quantity> <price>\n\t- print\n\t- exit\n");
+            printf("Command list:\n\n\t- create <id> <username> <phone> <address>\n\t- insert <barcode> <name> <quantity> <price>\n\t- print <name/EAN OR 'orders' OR 'acounts' OR '*'>\n\t- order <id> <username> <EAN OR name> <quantity> <priority>\n\t- exit\n");
         }else 
-            printf("Cannot recognise command, type help\n"); // kanto kltro grapse sxolia TODO
+            printf("Cannot recognise command, type help\n"); 
     }
 
     return 0;   
